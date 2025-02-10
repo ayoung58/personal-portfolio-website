c4f1123513990f76a28da7ffd1f93f8fabff2cfb
Theinterna <yalvin58@gmail.com>
2025-02-09 19:10:44 -0800

    Add a useEffect to detect when user scrolls past the navbar banner.
    Add a useState to check which navigation bar link is pressed and what "page" the user is on.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,50 @@
+import {useState, useEffect} from 'react';
 import {Navbar, Container} from "react-bootstrap";
 
 export const Navbar = () => {
+    // This link tracks which page we are currently on (and which link is currently active)
+    const {activeLink, setActiveLink} = useState('home');
+    // State to see if the user has scrolled (if they did, then the background would change)
+    const {scrolled, setScrolled} = useState(false);
+    const bannerHeight = 50;
+
+    // Fired when the user scrolls
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > bannerHeight) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        }
+
+        window.addEventListener("scroll", onScroll);
+
+        // Remove the event listener when the component is removed from the DOM
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [])
+
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
+    
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        // change the className based on whether the user scrolled the webpage or not
+        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
           <Container>
             <Navbar.Brand href="#home">
                 {/* We can have a name here or an image, like a logo*/}
                 <img src={''} alt="Logo" /> {/* TODO add source */}
             </Navbar.Brand>
             {/* When image is on the mobile menu and minimized, so we can add an icon here */}
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle aria-controls="basic-navbar-nav">
                 <span className="navbar-toggler-icon"></span>
+            </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#skills">Skills</Nav.Link>
-                <Nav.Link href="#Projects">Projects</Nav.Link>
+                <Nav.Link href="#home" className={activeLink === "home" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                <Nav.Link href="#skills" className={activeLink === "skills" ? "active navbar-link" : "navbar-link"} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+                <Nav.Link href="#Projects" className={activeLink === "projects" ? "active navbar-link" : "navbar-link" onClick={() => onUpdateActiveLink('projects')}}>Projects</Nav.Link>
                 {/* No need for a dropdown menu for now */}
                 {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
