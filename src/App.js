ebf62b0571bf017e54f8a0dcc57517c52a76db1a
Theinterna <yalvin58@gmail.com>
2025-02-19 15:22:12 -0800

    Add in placeholder code for Contact and its CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import {NavBar} from "./components/NavBar";
 import {Banner} from "./components/Banner";
 import {Skills} from "./components/Skills";
-import {Projects} from "./components/Projects"
+import {Projects} from "./components/Projects";
+import {Contact} from "./components/Contact";
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -15,6 +16,7 @@ function App() {
         <Banner />
         <Skills />
         <Projects />
+        <Contact />
       </div>
     </BrowserRouter>
   );
