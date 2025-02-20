dfa434894d5a3c2e0b8190456cbbbc736986f645
Theinterna <yalvin58@gmail.com>
2025-02-19 17:07:50 -0800

    Adjust styling for banner and footer

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -81,7 +81,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 {/* text changes with state */}
-                <h1>{`Hi! I'm Alvin`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                <h1>{`Hi! I'm Alvin`} <div className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></div></h1>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                     <HashLink className="text-decoration-none" to='#connect'>
                     <button onClick={() => console.log('connect')}><span>Let’s Connect</span> <ArrowRightCircle size={25} /></button>
